a51efc8e3334a88ea8abf05a1fbd05deea1d8870
KhizarAlam20 <khizaralam20@gmail.com>
2023-09-12 13:31:56 -0700

    v 1

--- Main.java
+++ Main.java
@@ -1,12 +1,31 @@
 package SCD.GUI_TASK.NOTE_TAKER;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
     Main(){
-        setSize(900, 400);
-        setLocation(350, 200);
+        setBounds(400,40,550,650);
         setLayout(null);
+        setResizable(false);
+
+        JPanel p = new JPanel();
+        p.setBackground(Color.black);
+        p.setBounds(0,0,550,650);
+        add(p);
+
+
+        JButton b = new JButton("GO");
+        b.setFont(new Font("AERIAL", Font.PLAIN, 30));
+        b.setBackground(Color.red);
+        b.setBorderPainted(false);
+        b.setSize(150,100);
+//        b.setBounds(50,40,70,30);
+        p.add(b);
+
+
+
+
         setVisible(true);
     }
     public static void main(String[] args) {
