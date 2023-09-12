7643425aa59166cc9cabbdd6ca9af8e90c51cae3
KhizarAlam20 <khizaralam20@gmail.com>
2023-09-12 13:17:55 -0700

    Swing app

---
+++ Main.java
@@ -0,0 +1,15 @@
+package SCD.GUI_TASK.NOTE_TAKER;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+    Main(){
+        setSize(900, 400);
+        setLocation(350, 200);
+        setLayout(null);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Main();
+    }
+}
